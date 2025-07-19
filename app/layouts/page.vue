d2a8187826430ca17d96a8201014aa627131fad4
Brian <brian.purnama@mhs.unsoed.ac.id>
2025-07-20 00:59:59 +0700

    add moduls and setup layout

---
+++ app/layouts/page.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="bg-gray-50 text-gray-800 dark:bg-gray-950 dark:text-gray-100 min-h-screen">
+    <header class="border-b border-gray-500/50">
+      <div></div>
+    </header>
+    <main>
+      <slot />
+    </main>
+    <footer></footer>
+  </div>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+  definePageMeta({
+    layout: 'page',
+  })
+</script>
+
+<template>
+  <div>
+    <h1>Halo</h1>
+  </div>
+</template>
